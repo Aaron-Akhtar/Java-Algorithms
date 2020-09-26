5b00141bc6ebde218d237cf705ba828fbbd74064
Aaron Akhtar <52239843+Aaron-Akhtar@users.noreply.github.com>
2020-09-26 04:54:21 +0100

    Loops over all ip addresses in the world apart from private ones.

---
+++ LoopOverAllInetAddresses.java
@@ -0,0 +1,19 @@
+/*
+Loops over every IP in the world 
+*/
+class Main {
+    public static void main(String[] args) {
+        for (int x = 0; x < 256; x++) {
+            if (x == 10) continue;
+            for (int f = 0; f < 256; f++) {
+                if ((x == 172 && (f >= 16 && f <= 31)) ||
+                    (x == 192 && f == 168)) continue;
+                for (int d = 0; d < 256; d++) {
+                    for (int g = 0; g < 256; g++) {
+                        //do
+                    }
+                }
+            }
+        }
+    }
+}
